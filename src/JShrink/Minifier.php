63e20aa1e8406ab69a3758f45f6aac7e53ede7ed
Robert Hafner <tedivm@tedivm.com>
2014-01-14 23:12:59 -0800

    Updated header comments.

--- src/JShrink/Minifier.php
+++ src/JShrink/Minifier.php
@@ -1,45 +1,19 @@
 <?php
+/*
+ * This file is part of the JShrink package.
+ *
+ * (c) Robert Hafner <tedivm@tedivm.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 /**
  * JShrink
  *
- * Copyright (c) 2009-2012, Robert Hafner <tedivm@tedivm.com>.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *
- *   * Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in
- *     the documentation and/or other materials provided with the
- *     distribution.
- *
- *   * Neither the name of Robert Hafner nor the names of his
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
  *
  * @package    JShrink
  * @author     Robert Hafner <tedivm@tedivm.com>
- * @copyright  2009-2012 Robert Hafner <tedivm@tedivm.com>
- * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
- * @link       https://github.com/tedivm/JShrink
- * @version    Release: 0.5.1
  */
 
 namespace JShrink;
@@ -51,9 +25,9 @@ namespace JShrink;
  * Usage - Minifier::minify($js, $options);
  * Usage - Minifier::minify($js, array('flaggedComments' => false));
  *
- * @package	JShrink
- * @author	Robert Hafner <tedivm@tedivm.com>
- * @license	http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @package JShrink
+ * @author Robert Hafner <tedivm@tedivm.com>
+ * @license http://www.opensource.org/licenses/bsd-license.php  BSD License
  */
 class Minifier
 {
