eb94245537e9eb8f050c082008c276e4c8d9fb83
Robert Hafner <tedivm@tedivm.com>
2014-01-29 15:26:29 -0800

    Updated test wrapper for more readable output

--- tests/runTests.sh
+++ tests/runTests.sh
@@ -4,6 +4,9 @@ set -e
 echo 'Running unit tests.'
 phpunit --verbose --coverage-text
 
+echo ''
+echo ''
+echo ''
 echo 'Testing for Coding Styling Compliance.'
 echo 'All code should follow PSR standards.'
-./vendor/fabpot/php-cs-fixer/php-cs-fixer fix ./src --dry-run --level="all" -vv
+./vendor/fabpot/php-cs-fixer/php-cs-fixer fix ./ --dry-run --level="all" -vv
