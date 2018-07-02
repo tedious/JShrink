254e354e6fa22f3728d9a80276aa9df0f44e61b7
Alex <aprogs@2281272.no-reply.drupal.org>
2018-07-02 17:38:39 +0300

    Fixed code formatting notices.

--- src/JShrink/Minifier.php
+++ src/JShrink/Minifier.php
@@ -274,9 +274,8 @@ class Minifier
         if (isset($this->c)) {
             $char = $this->c;
             unset($this->c);
-
-            // Otherwise we start pulling from the input.
         } else {
+            // Otherwise we start pulling from the input.
             $char = substr($this->input, $this->index, 1);
 
             // If the next character doesn't exist return false.
@@ -460,7 +459,7 @@ class Minifier
 
         // Loop until the string is done
         // Grab the very next character and load it into a
-        while (($this->a = $this->getChar()) !== FALSE) {
+        while (($this->a = $this->getChar()) !== false) {
             switch ($this->a) {
 
                 // If the string opener (single or double quote) is used
