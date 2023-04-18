bab7c39eff9a84e8cb117e7b7a3184710c5b5186
Robert Hafner <tedivm@tedivm.com>
2023-04-18 17:25:31 -0500

    Support private methods
    resolves #131

---
+++ tests/Resources/jshrink/input/private_methods.js
@@ -0,0 +1,30 @@
+export default class Example {
+  #prop
+
+  set prop (value) {
+    this.#prop = value
+
+    this.#event(value)
+  }
+  get prop () {
+    return this.#prop
+  }
+
+  constructor (arg) {
+    this.#prop = arg
+
+    if (arg) {
+      this.#method()
+    }
+  }
+
+  #event = f => {
+    if (typeof f === 'function') {
+      f()
+    }
+  }
+
+  #method = async _ => {
+    return fetch(param)
+  }
+}
